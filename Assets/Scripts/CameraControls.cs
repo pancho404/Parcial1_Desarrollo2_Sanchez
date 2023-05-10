1511611a876a4782cbf725f9c581651263c97692
Francisco <franciscosanchez2101@gmail.com>
2023-05-10 12:35:00 -0300

    Updated Camera Controls.

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class CameraControls : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject feet;
     [SerializeField] private float speed = 0.2f;
     [SerializeField] private AnimationCurve rotationCurve;
     [SerializeField] private Rigidbody rigidbody;
@@ -20,11 +20,13 @@ public class CameraControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cameraPositions[0] = new Vector3(0, 2, -10);
-        cameraPositions[1] = new Vector3(10, 2, -0.5f);
-        cameraPositions[2] = new Vector3(0, 2, 10);
-        cameraPositions[3] = new Vector3(-10, 2, 0);
-
+        cameraPositions[0] = feet.transform.localPosition;
+        cameraPositions[1] = cameraPositions[0] + new Vector3(15, 0, 0);
+        cameraPositions[2] = cameraPositions[1] + new Vector3(-15, 0, 15);
+        cameraPositions[3] = cameraPositions[2] + new Vector3(15, 0, -15);
+        Debug.Log(feet.transform.localPosition);
+        Debug.Log(transform.localPosition);
+        Debug.Log(cameraPositions[0]);
         nextPosition = cameraPositions[1];
         previousPosition = cameraPositions[3];
     }
@@ -32,33 +34,24 @@ public class CameraControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cameraPositions[0] = player.transform.forward - new Vector3(0, 2, 10);
-        cameraPositions[1] = player.transform.right + new Vector3(10, 2, 0);
-        cameraPositions[2] = player.transform.forward + new Vector3(0, 2, 10);
-        cameraPositions[3] = player.transform.right - new Vector3(10, 2, 0);
         //rotationValue = rotationCurve.Evaluate(speed * Time.deltaTime);
-        transform.LookAt(player.transform.position);
+        transform.LookAt(feet.transform.position);
     }
 
     void OnRotateCameraLeft(InputValue input)
     {
-        nextPosition = cameraPositions[counter];
         previousPosition = cameraPositions[counter];
-        counter++;
-        if (counter >= 3)
+        counter--;
+        if (counter == 3)
         {
             counter = 0;
         }
         StartCoroutine(SlerpCamera(previousPosition));
-        if (!input.isPressed)
-        {
-            //StopAllCoroutines();
-        }
+        StopCoroutine(SlerpCamera(previousPosition));
+
 
         // transform.RotateAround(player.transform.position, player.transform.up, 90.0f);
 
-        counter--;
-        rigidbody.MoveRotation(Quaternion.Euler(0, -90, 0));
     }
     void OnRotateCameraRight(InputValue input)
     {
@@ -70,24 +63,26 @@ public class CameraControls : MonoBehaviour
             counter = 0;
         }
         StartCoroutine(SlerpCamera(nextPosition));
-        StopCoroutine(SlerpCamera(nextPosition));        
+        StopCoroutine(SlerpCamera(nextPosition));
     }
 
     private IEnumerator SlerpCamera(Vector3 position)
     {
         var now = Time.time;
         var start = Time.time;
-        while (transform.position != position)
+        Vector3 distance = position - transform.position;
+        float duration = distance.magnitude / speed;
+        while (now - start < duration)
         {
-            transform.position = Vector3.Slerp(transform.position, position, ((now - start) / speed) * Time.deltaTime);
             now = Time.time;
+            transform.position = Vector3.Slerp(transform.position, position, (now - start) / duration);
             yield return null;
         }
         transform.position = position;
 
     }
 
-   
-     
-    
+
+
+
 }
